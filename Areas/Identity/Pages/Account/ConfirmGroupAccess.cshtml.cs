2437a9723cc8257d6d4db77541c03158af1be750
kylescudder <74150974+kylescudder@users.noreply.github.com>
2021-05-26 18:21:27 +0100

    Invite rejection now displays correct text and title

--- Areas/Identity/Pages/Account/ConfirmGroupAccess.cshtml.cs
+++ Areas/Identity/Pages/Account/ConfirmGroupAccess.cshtml.cs
@@ -30,10 +30,6 @@ namespace DatePot.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnGetAsync(string userId, string chosenUserId, string UserGroupID, string response)
         {
-            // if (userId == null || chosenUserId == null || UserGroupID == null || response == null)
-            // {
-            //     return RedirectToPage("/Index");
-            // }
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
             {
@@ -41,8 +37,12 @@ namespace DatePot.Areas.Identity.Pages.Account
             }
             if (response.ToLower() == "true") {
                 await _siteData.AcceptInvite(chosenUserId, Convert.ToInt32(UserGroupID));
+                ViewData["ResponseText"] = "Thanks for accepting the invite! Now you can share all the best things with the people you hold dearest! ☮";
+                ViewData["Title"] = "Accept invite";
             } else {
                 await _siteData.RejectInvite(chosenUserId, Convert.ToInt32(UserGroupID), userId);
+                ViewData["ResponseText"] = "Sad times my friend, sorry to see you reject it, but respect your choice. 🤘";
+                ViewData["Title"] = "Reject invite";
             }
             return Page();
         }
